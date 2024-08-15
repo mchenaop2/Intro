bea2463cbe5d60dbca8b7ec86d35b614cf5a62d5
mchenaop2 <mchenaop2@eafit.edu.co>
2024-08-15 10:08:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+from PIL import Image
+
 st.title ("Hello")
+
 st.header("Peopleeee")
 st.write("Amo viajar")
+image = Image.open("Playa.JPG")
+
+st.image(image, caption="Beach")
